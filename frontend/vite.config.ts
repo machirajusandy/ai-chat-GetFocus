b1f048639c477c9aa6c0fe0ca603482c95d36095
machirajusandy <58855274+machirajusandy@users.noreply.github.com>
2024-11-09 12:38:08 +0100

    add vitest ignore e2e test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   },
   test: {
     environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts']
+    setupFiles: ['./vitest.setup.ts'],
+    exclude: ['**/e2e/**', '**/node_modules/**']
   }
 })
